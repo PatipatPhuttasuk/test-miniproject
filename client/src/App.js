61b7175b94c0902a38bb662e0ae93ac84db72d78
Patipat <5835512104@psu.ac.th.com>
2020-04-30 21:28:23 +0700

    add login

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import StudentList from './components/student/StudentList'
-import InputForm from './components/student/InputForm';
-import Nevbar from './components/nevbar/menu'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import Login from "./components/Login";
+import StudentList from "./components/student/StudentList";
+import InputForm from "./components/student/InputForm";
+import Nevbar from "./components/nevbar/menu";
+import "./App.css";
+import auth from './firebase';
 
-const App = () => {
+function App() {
+  const [session, setSession] = useState({
+    isLoggedIn: false,
+    currentUser: null,
+    errorMessage: null
+  });
+  useEffect(() => {
+    const handleAuth = auth.onAuthStateChanged(user => {
+      if (user) {
+        setSession({
+          isLoggedIn: true,
+          currentUser: user,
+          errorMessage: null
+        });
+      }
+    });
+    return () => {
+      handleAuth();
+    }
+  }, [])
+const handleLogout = () => {
+  auth.signOut().than(response => {
+    setSession({
+      isLoggedIn: false,
+      currentUser: null
+    });
+  });
+}
   return (
-    <div className='body-App'>
-      <Nevbar />
-      <h1>Insert Data</h1>
-      <InputForm />
-      <h1>Students Infomation</h1>
-      <StudentList />
+    <div className="body-App">
+      {session.isLoggedIn ? (
+        ((<Nevbar />),
+        (<h1>Insert Data</h1>),
+        (<InputForm />),
+        (<h1>Students Infomation</h1>),
+        (<StudentList />))
+        ,<h1>Ahoy!, {session.currentUser && session.currentUser.email}</h1>
+        ,<button type="button" onClick={handleLogout}></button>
+      ) : (
+        <Login setSession={setSession} />
+      )}
     </div>
   );
 }
---
+++ client/src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAVQXgDX-_xEexgNoiSr-xu42AppIA9w3E",
+  authDomain: "miniproject-v13.firebaseapp.com",
+  databaseURL: "https://miniproject-v13.firebaseio.com",
+  projectId: "miniproject-v13",
+  storageBucket: "miniproject-v13.appspot.com",
+  messagingSenderId: "844451302535",
+  appId: "1:844451302535:web:3bf205b9797eaf09037c22",
+  measurementId: "G-T94N80EQRP",
+};
+
+export default firebaseConfig;
